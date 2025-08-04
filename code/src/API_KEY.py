89699d311db193723a5e565d3ad62a38ba627a8d
shee333 <101503129+shee333@users.noreply.github.com>
2025-08-04 13:47:42 +0800

    Update API_KEY.py

--- code/src/API_KEY.py
+++ code/src/API_KEY.py
@@ -2,19 +2,19 @@
 
 # 通义千问QwenVL系列
 # https://bailian.console.aliyun.com/#/model-market
-Qwen_KEY = "sk-39e69b06c77440eaa7a1be063b42a520"
+Qwen_KEY = "sk-39e69b06cPleaseChangeThis"
 
 
 # 零一万物大模型开放平台
 # https://platform.lingyiwanwu.com
-YI_KEY = "9692e9b8c88647d29bd2f87a4d6d55c9"
+YI_KEY = "9692e9b8c88PleaseChangeThis"
 
 
 # 百度智能云千帆ModelBuilder
 # https://qianfan.cloud.baidu.com
-QIANFAN_ACCESS_KEY = "ALTAKEeZox6wPS6rbbYzTOjYyP"
-QIANFAN_SECRET_KEY = "a54544057eb1414a8465e78f4d5b3f87"
+QIANFAN_ACCESS_KEY = "ALTAKEeZox6wPPleaseChangeThisP"
+QIANFAN_SECRET_KEY = "a54544057eb1414a8465PleaseChangeThis"
 
 # 百度智能云千帆AppBuilder-SDK
-APPBUILDER_TOKEN = "bce-v3/ALTAK-CSjOLgqLLjRb9zh9CCagw/7c475fb4cf9af089e4233e3b9bff48d59766b80f"
+APPBUILDER_TOKEN = "bce-v3/APleaseChangeThish9PleaseChangeThisf9af089PleaseChangeThis"
 
