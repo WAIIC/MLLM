33a9c08e8331e5a3da13a83b3013e93ca78c776d
shee333 <101503129+shee333@users.noreply.github.com>
2025-08-12 11:42:37 +0800

    Update utils_agent.py

--- code/src/utils_agent.py
+++ code/src/utils_agent.py
@@ -7,7 +7,7 @@ AGENT_SYS_PROMPT = '''
 
 '''
 
-def agent_plan(PROMPT='先回到原点，再把LED灯改为墨绿色，然后把绿色方块放在篮球上'):
+def agent_plan(PROMPT=''):
     print('Agent智能体编排动作')
     agent_plan = llm_yi(PROMPT)
     return agent_plan
